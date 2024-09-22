694af8e0d98f66e57dd7697d8bf2333dd95eec9f
Yarik2019 <yroslavterno@gmail.com>
2024-09-22 22:01:24 +0300

    'ass'

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -3,7 +3,7 @@ import s from "./Loader.module.css";
 const Loader = () => {
   return (
     <div className={s.loader}>
-      <Vortex height="300" width="300" ariaLabel="loading" />
+      <Vortex height="100" width="100" ariaLabel="loading" />
     </div>
   );
 };
