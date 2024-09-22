5f9b50824a77b3bbf7e5aee809ed1667c1527a54
Yarik2019 <yroslavterno@gmail.com>
2024-09-22 21:47:05 +0300

    fes

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,67 @@
 import "./App.css";
-// import { useState, useEffect } from "react";
-// import { fetchGallery } from "./services/api";
-// import toast from "react-hot-toast";
-// import SearchBar from "./components/SearchBar/SearchBar";
-// import ImageGallery from "./components/ImageGallery/ImageGallery";
-// import Loader from "./components/Loader/Loader";
-// import ErrorMessage from "./components/ErrorMessage/ErrorMessage";
-// import LoadMoreBtn from "./components/LoadMoreBtn/LoadMoreBtn";
-// import ImageModal from "./components/ImageModal/ImageModal";
+import { useState, useEffect } from "react";
+import { fetchGallery } from "./services/api";
+import toast from "react-hot-toast";
+import SearchBar from "./components/SearchBar/SearchBar";
+import ImageGallery from "./components/ImageGallery/ImageGallery";
+import Loader from "./components/Loader/Loader";
+import ErrorMessage from "./components/ErrorMessage/ErrorMessage";
+import LoadMoreBtn from "./components/LoadMoreBtn/LoadMoreBtn";
+import ImageModal from "./components/ImageModal/ImageModal";
 function App() {
-  // const [gallerys, setGallerys] = useState([]);
-  // const [isLoading, setIsLoading] = useState(false);
-  // const [isError, setIsError] = useState(null);
-  // const [query, setQuery] = useState("");
-  // const [page, setPage] = useState(1);
-  // const [modalImage, setModalImage] = useState(null);
-  // const [showModal, setShowModal] = useState(null);
+  const [gallerys, setGallerys] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(null);
+  const [query, setQuery] = useState("");
+  const [page, setPage] = useState(1);
+  const [modalImage, setModalImage] = useState(null);
+  const [showModal, setShowModal] = useState(null);
 
-  // useEffect(() => {
-  //   if (!query) {
-  //     return;
-  //   }
+  useEffect(() => {
+    if (!query) {
+      return;
+    }
 
-  //   const getData = async () => {
-  //     try {
-  //       setIsLoading(true);
-  //       setIsError(null);
+    const getData = async () => {
+      try {
+        setIsLoading(true);
+        setIsError(null);
 
-  //       const data = await fetchGallery(query, page);
-  //       setGallerys((prev) => [...prev, ...data.data.results]);
-  //     } catch (err) {
-  //       setIsError(`Error message: ${err}`);
-  //     } finally {
-  //       setIsLoading(false);
-  //     }
-  //   };
+        const data = await fetchGallery(query, page);
+        setGallerys((prev) => [...prev, ...data.data.results]);
+      } catch (err) {
+        setIsError(`Error message: ${err}`);
+      } finally {
+        setIsLoading(false);
+      }
+    };
 
-  //   getData();
-  // }, [query, page]);
+    getData();
+  }, [query, page]);
 
-  // const handleSearch = (searchQuery) => {
-  //   if (!searchQuery) {
-  //     toast.error("Будь ласка, введіть пошуковий текст");
-  //     return;
-  //   }
-  //   setQuery(searchQuery);
-  //   setGallerys([]);
-  //   setPage(1);
-  // };
-  // const openModal = (image) => {
-  //   setModalImage(image);
-  //   setShowModal(true);
-  // };
-  // const closeModal = () => {
-  //   setShowModal(false);
-  //   setModalImage(null);
-  // };
+  const handleSearch = (searchQuery) => {
+    if (!searchQuery) {
+      toast.error("Будь ласка, введіть пошуковий текст");
+      return;
+    }
+    setQuery(searchQuery);
+    setGallerys([]);
+    setPage(1);
+  };
+  const openModal = (image) => {
+    setModalImage(image);
+    setShowModal(true);
+  };
+  const closeModal = () => {
+    setShowModal(false);
+    setModalImage(null);
+  };
 
-  // const loadMoreImages = () => setPage((prevPage) => prevPage + 1);
+  const loadMoreImages = () => setPage((prevPage) => prevPage + 1);
 
   return (
     <>
-      {/* <SearchBar onSearch={handleSearch} />
+      <SearchBar onSearch={handleSearch} />
       {isError && <ErrorMessage message={isError} />}
       {gallerys.length > 0 && (
         <ImageGallery gallerys={gallerys} openModal={openModal} />
@@ -72,8 +72,7 @@ function App() {
         <LoadMoreBtn loadMoreImages={loadMoreImages} />
       )}
       {isLoading && <Loader />}
-      {showModal && <ImageModal image={modalImage} closeModal={closeModal} />} */}
-      hello
+      {showModal && <ImageModal image={modalImage} closeModal={closeModal} />}
     </>
   );
 }
